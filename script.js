5a6603255a67d27095d0ef77fb88ee8b73392db1
Chase Palmer <chase.palmer012704@gmail.com>
2023-03-29 23:51:24 -0400

    added slider and buttons on bottom, and cleaned up some javascript code

--- script.js
+++ script.js
@@ -1,25 +1,12 @@
+const gridSize = 512;
+const defaultNumBlocks = 32;
+
+let mouseDown = false;
+let numBlocks = defaultNumBlocks;
+let color = 'black';
+
 const grid = document.querySelector('#grid-capsule');
-
-for (let i = 0; i < 16; i++) {
-    var rowDiv = document.createElement('div');
-    rowDiv.className = 'row';
-    grid.appendChild(rowDiv);
-    for (let j = 0; j < 16; j++) {
-        var colDiv =  document.createElement('div');
-        colDiv.className = 'grid-element';
-        rowDiv.appendChild(colDiv);
-    }
-}
-
-const gridElement = document.querySelectorAll('.grid-element');
-
-gridElement.forEach(item => {
-    item.addEventListener('mouseover', () => {
-        item.style.backgroundColor = 'white';
-    });
-  });
-
-
+const resetButton = document.querySelector('#reset');
 
 function reset() {
     gridElement.forEach(item => {
@@ -27,8 +14,46 @@ function reset() {
     })
 }
 
-const resetButton = document.querySelector('#reset');
+for (let i = 0; i < defaultNumBlocks; i++) {
+    var rowDiv = document.createElement('div');
+    rowDiv.className = 'row';
+    rowDiv.style.height = '100%';
+    grid.appendChild(rowDiv);
+    for (let j = 0; j < defaultNumBlocks; j++) {
+        const colDiv =  document.createElement('div');
+        colDiv.className = 'grid-element';
+        setBoxSize(colDiv);
+        rowDiv.appendChild(colDiv);
+    }
+}
+
+const gridElement =  document.querySelectorAll('.grid-element');
+document.body.onmouseup = () => {
+    mouseDown = false;
+}
+
+gridElement.forEach(item => {
+    item.addEventListener('mousedown', () => {
+        mouseDown = true;
+        item.style.backgroundColor = 'white';
+    });
+    item.addEventListener('mouseover', () => {
+        if (mouseDown) {
+            item.style.backgroundColor = 'white';
+        }
+    }); 
+}); 
+
+function setBoxSize(item) {
+    let itemCalc = gridSize/numBlocks;
+    let itemSize = (itemCalc/gridSize) * 100;
+    item.style.width = `${itemSize}%`;
+    item.style.height = `$100%`;
+}
 
 resetButton.addEventListener('click', () => {
     reset();
 });
+
+window.onload = () => {
+}
